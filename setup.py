d1db530252865321594c6b81812eb79c508c5217
Gian U. Uy <gianu@marketamerica.com>
2017-09-11 15:08:52 -0400

    More setup
    - Corrected the requirements.txt file
    - setup.py to put the version

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='saim-python-sdk',
+    version='1.0.0',
+    packages=['tests'],
+    url='',
+    license='',
+    author='SAIM Python Group',
+    author_email='',
+    description=''
+)
